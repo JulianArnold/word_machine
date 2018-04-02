21d73fa241a0616a49f72724e228a5882d4a89df
JulianArnold <julian_arnold@hotmail.com>
2018-04-02 11:09:12 +0100

    learned to write a site layout test; html-driven mainly, an app helper test inc in test_helper

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    assert_equal full_title,              'Word Machine Blog'
+    assert_equal full_title('Help'),      'Help | Word Machine Blog'
+    assert_equal full_title('About'),     'About | Word Machine Blog'
+    assert_equal full_title('Contact'),   'Contact | Word Machine Blog'
+  end
+end
